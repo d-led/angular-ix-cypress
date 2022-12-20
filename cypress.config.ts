268264acd503ee606cab12466f2978765dc60a0e
Dmitry Ledentsov <dmitry.ledentsov@siemens.com>
2022-12-20 16:13:40 +0100

    cypress e2e config

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'cypress/results/report-app-spec-[hash].xml',
+      toConsole: false,
+    },
+    specPattern: [
+      'cypress/e2e/**.cy.ts'
+    ],
+    supportFile: false,
+    setupNodeEvents: async (on, config) => {
+      // Make sure to return the config object as it might have been modified by the plugin.
+      return config;
+    },
+  },
+});
